474eb31b99124cb7f788b5d76d74bca02edd4b49
Hakurunai <guillaumeal.pro@gmail.com>
2024-06-19 11:10:36 +0200

    Base character class

---
+++ Source/Aura/Aura.cpp
@@ -0,0 +1,6 @@
+// Copyright Hakurunai
+
+#include "Aura.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Aura, "Aura" );
---
+++ Source/Aura/Aura.h
@@ -0,0 +1,6 @@
+// Copyright Hakurunai
+
+#pragma once
+
+#include "CoreMinimal.h"
+
