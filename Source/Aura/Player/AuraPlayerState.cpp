90b0b981408dad2a5444e402d66082deca0c8b83
Hakurunai <guillaumeal.pro@gmail.com>
2024-09-20 16:15:28 +0200

    Adding our playerState class

---
+++ Source/Aura/Player/AuraPlayerState.cpp
@@ -0,0 +1,10 @@
+// Copyright Hakurunai
+
+
+#include "AuraPlayerState.h"
+
+AAuraPlayerState::AAuraPlayerState()
+{
+	//Frequency to update this object on the server (server will try to be at this value)
+	NetUpdateFrequency = 100.f;
+}
