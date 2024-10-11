fccede0d1d4771147a1875438ef7cc875c1d11dc
Hakurunai <guillaumeal.pro@gmail.com>
2024-10-11 12:01:27 +0200

    Adding another type of object : this time with a periodic effect who will be removed after a fixed period of time

--- Source/Aura/Actor/AuraEffectActor.h
+++ Source/Aura/Actor/AuraEffectActor.h
@@ -20,6 +20,9 @@ protected:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Effects")
 	TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
 
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Effects")
+	TSubclassOf<UGameplayEffect> DurationGameplayEffectClass;
+
 	UFUNCTION(BlueprintCallable)
 	void ApplyEffectToTarget(AActor* TargetActor, TSubclassOf<UGameplayEffect> GameplayEffectClass);
 };
